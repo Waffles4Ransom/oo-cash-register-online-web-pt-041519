ebf69096179b530ff919b4ca676ea53d75ff4b62
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-18 18:51:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
 
+  def add_item(title, price, quantity = 1)
+    @total += price*quantity
+  end
+  
+  def apply_discount
+    self.total*discount
+  end
+end
