6fd7e024cc9b5d456f86d3bab81e1e5bd1dc71ba
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-18 19:56:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
     if @discount > 0 
       after_discount = self.total - (self.total*(discount.to_f/100)).to_i
       self.total -= after_discount
-      return "After the discount, the total comes to #{self.total}."
+      return "After the discount, the total comes to #{after_discount}."
     else 
       return "There is no discount to apply."
     end
