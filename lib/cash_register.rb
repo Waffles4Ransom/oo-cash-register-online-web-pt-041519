fa0e8ed96b784254b77cb7134518fcd877555ac4
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-18 19:00:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,9 +14,9 @@ class CashRegister
   
   def apply_discount
     if discount != 0 
-      discount
+      discount/100
     else 
-      "There is no discount to apply."
+      puts "There is no discount to apply."
     end
   end
   
